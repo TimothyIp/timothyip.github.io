1ec68f0abfdc7dfdeb851773c937f6a810bb55a9
Timothy Ip <timothy.ip4@gmail.com>
2017-08-29 18:12:19 -0400

    Fixed javascript to refresh javascript

--- gulpfile.js
+++ gulpfile.js
@@ -2,40 +2,45 @@ const gulp = require("gulp");
 const sass = require("gulp-sass");
 const concat = require("gulp-concat");
 const babel = require("gulp-babel");
+const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 
 
 gulp.task('styles', () => {
   return gulp.src('./dev/styles/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer('last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
     .pipe(concat('styles.css'))
     .pipe(gulp.dest('./public/styles/'))
     .pipe(browserSync.reload({
-    	stream: true
+      stream: true
     }))
 });
 
 gulp.task('scripts', () => {
-	return gulp.src('./dev/scripts/*.js')
-	 .pipe(babel({
-	 	presets:['es2015']
-	 }))
-	 .pipe(gulp.dest('./public/scripts'))
+  return gulp.src('./dev/scripts/*.js')
+   .pipe(babel({
+    presets:['es2015']
+   }))
+   .pipe(gulp.dest('./public/scripts'))
+   .pipe(browserSync.reload({
+     stream: true
+   }))
 });
 
 gulp.task('html', () => {
-	return gulp.src('./dev/*.html')
-	 .pipe(gulp.dest('./public/'))
-	 .pipe(browserSync.reload({
-	 	stream: true
-	 }))
-})
+  return gulp.src('./dev/*.html')
+   .pipe(gulp.dest('./public/'))
+   .pipe(browserSync.reload({
+    stream: true
+   }))
+});
 
-gulp.task('watch',["browserSync", "styles", "scripts","html"], () => {
-	gulp.watch("./dev/*.html", ["html"], browserSync.reload);
-	gulp.watch("./dev/styles/**/*.scss", ['styles']);
-	gulp.watch("./dev/scripts/*.js", browserSync.reload,['scripts']);
-})
+gulp.task('watch',["browserSync", "styles"], () => {
+  gulp.watch("./dev/*.html", ["html"], browserSync.reload);
+  gulp.watch("./dev/styles/**/*.scss", ['styles']);
+  gulp.watch("./dev/scripts/*.js", ['scripts'], browserSync.reload);
+});
 
 gulp.task('browserSync', function() {
   browserSync.init({
@@ -50,4 +55,4 @@ gulp.task('browserSync', function() {
   })
 });
 
-gulp.task('default',['watch','styles','scripts','browserSync']);
+gulp.task('default',['watch','styles','scripts']);
